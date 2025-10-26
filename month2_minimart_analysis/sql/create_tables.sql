9eca6812d9fe9fae87d2d5d39509b7d5938ed9b9
Muminah <105560838+mumi-nah@users.noreply.github.com>
2025-10-26 23:18:54 +0100

    Update create_tables.sql

--- month2_minimart_analysis/sql/create_tables.sql
+++ month2_minimart_analysis/sql/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL script to create necessary tables
+CREATE TABLE customers(  
+customer_id, INTEGER PRIMARY KEY
+name TEXT,
+email varchar(50),
+join_date DATE
+);
+
+
+CREATE TABLE products(
+product_id INTEGER PRIMARY KEY,
+product_name TEXT,
+category VARCHAR,
+price NUMERIC
+);
+
+CREATE TABLE orders (
+order_id INTEGER PRIMARY KEY,
+customer_id INTEGER,
+product_id INTEGER,
+quantity INTEGER,
+order_date TEXT,
+FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY(product_id) REFERENCES products(product_id)
